8b53acd2c2e80c444c6fa98478c51e8c098f9fcb
Pablo75321 <pablodanilo75321@gmail.com>
2019-08-15 15:11:40 -0300

    Challenge-04

--- challenge-4/src/challenges/index.js
+++ challenge-4/src/challenges/index.js
@@ -1,7 +1,34 @@
 console.time("Performance")
+const clients = require('../db/index')
+
 /* ----- 1 ----- */
 
+const discountsClient = clients.map(({ name, orderValue, discounts }) => {
+  return {
+    name,
+    orderValue,
+    discounts: discounts.sort((firstOrder, secondOrder) =>
+      firstOrder.order > secondOrder.order && 1)
+  }
+})
+
+// console.dir(discountsClient, { depth: null })
+
 /* ----- 2 ----- */
+
+const totalDiscounts = discountsClient.map(({ name, orderValue, discounts }) => {
+  return {
+    name,
+    orderValue,
+    resultantDiscount: [...discounts].map(({ values }) => {
+      return values.reduce((firstValue, secondValue) => {
+        return (100 - firstValue) * (100 - secondValue) / 100
+      }, '')
+    })
+  }
+})
+
+console.dir(totalDiscounts, { depth: null })
 
 /* ----- 3 ----- */
 
